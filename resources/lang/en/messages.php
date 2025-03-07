b1067da70d951a27551194a57a8b57e82ff1e1d4
Ahmed Naser <eng.ahmed.naserr@gmail.com>
2025-03-07 08:04:38 +0200

    Fixed response key case issue in ResponseHandler

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,4 +2,5 @@
 
 return [
     'rate_limited' => 'Too many requests. Please try again after 5 minutes.',
+    'success' => 'OTP sent successfully!',
 ];
--- src/Handlers/ResponseHandler.php
+++ src/Handlers/ResponseHandler.php
@@ -7,17 +7,18 @@ class ResponseHandler
     public static function handle($response)
     {
         $data = json_decode($response, true);
-
+        $data = array_change_key_case($data, CASE_LOWER);
         if ($data['code'] !== '4901' && $data['code'] !== '1901') {
             return [
                 'error' => true,
                 'message' => 'Error Code: ' . $data['code'],
             ];
         }
-
+    
         return [
             'error' => false,
             'data' => $data,
         ];
     }
+    
 }
